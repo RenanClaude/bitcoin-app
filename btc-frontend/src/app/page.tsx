1c009dae356d00d1baaf845bac61302a100ccce7
Renan <renanclaude.rcg@gmail.com>
2025-07-08 04:44:28 -0300

    Adding Dockerfile.dev for the frontend and backend, one for each. And also adapting docker-compose for development.

--- btc-frontend/src/app/page.tsx
+++ btc-frontend/src/app/page.tsx
@@ -3,7 +3,7 @@ import BtcPrice from "@/components/BtcPrice";
 
 export default function HomePage() {
   return (
-    <div className="flex flex-col items-center h-screen bg-background">
+    <div className="flex flex-col items-center h-screen bg-">
       <BtcPrice />
       <BtcIndicators />
     </div>
