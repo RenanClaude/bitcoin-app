bc19ffc6caf48ffdf8f6794e7b9582ddefd55c2f
Renan <renanclaude.rcg@gmail.com>
2025-07-10 17:32:46 -0300

    adjusting the btc-frontend and btc-backend dockerfiles to be production dockerfiles, and adjusting docker-compose accordingly.

--- btc-backend/src/domain/use-cases/BtcDailyPrice/delete-BtcDailyPrice.ts
+++ btc-backend/src/domain/use-cases/BtcDailyPrice/delete-BtcDailyPrice.ts
@@ -1,14 +1,14 @@
-// src/domain/use-cases/transaction/delete-transaction.ts
-import BtcDailyRepositoryInterface from '../../repositories/BtcDailyPrice-repository';
+// // src/domain/use-cases/transaction/delete-transaction.ts
+// import BtcDailyRepositoryInterface from '../../repositories/BtcDailyPrice-repository';
 
-export class DeleteBtcDailyPrice {
-  constructor(private readonly btcDailyRepository: BtcDailyRepositoryInterface) {}
+// export class DeleteBtcDailyPrice {
+//   constructor(private readonly btcDailyRepository: BtcDailyRepositoryInterface) {}
 
-  async execute(id: string): Promise<void> {
-    const priceAndDayToDelete = await this.btcDailyRepository.findById(id);
-    if (!priceAndDayToDelete) {
-      throw new Error('Price and date to delete not found');
-    }
-    await this.btcDailyRepository.delete(id);
-  }
-}
+//   async execute(id: string): Promise<void> {
+//     const priceAndDayToDelete = await this.btcDailyRepository.findById(id);
+//     if (!priceAndDayToDelete) {
+//       throw new Error('Price and date to delete not found');
+//     }
+//     await this.btcDailyRepository.delete(id);
+//   }
+// }
